1ce1161f8fbb63bf48be4495e16f9489f8369b01
let‘s go <115095961+toby7989@users.noreply.github.com>
2025-10-12 17:04:36 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -73,6 +73,27 @@ const siteIcon = {
   LaptopCode,
 };
 
+  // 可前往 https://www.xicons.org 自行挑选并在此处引入
+// 此处引入的是 fa 类型
+import {
+  Link,
+  Blog,
+  CompactDisc,
+  Cloud,
+  Compass,
+  Book,
+  Fire,
+  LaptopCode,
+} from "@vicons/fluent
+";
+
+...
+
+// 网站链接图标
+const siteIcon = {
+ AccessTime24Filled
+};
+  
 // 链接跳转
 const jumpLink = (data) => {
   if (data.name === "音乐" && store.musicClick) {
